435d2fe8b6b4c526a5c24bcabac97889563aa05b
tonimatasdev <79017964+TonimatasDEV@users.noreply.github.com>
2026-05-15 17:48:15 +0200

    chore: Add missing fields

--- network/packets/server/login/disconnect.go
+++ network/packets/server/login/disconnect.go
@@ -3,12 +3,13 @@ package login
 import "ethene/network/buffers"
 
 type Disconnect struct {
+	Reason string
 }
 
-func (p Disconnect) Id() int32 {
+func (p *Disconnect) Id() int32 {
 	return 0
 }
 
-func (p Disconnect) Marshal(_ buffers.NetworkBuffer) {
-
+func (p *Disconnect) Marshal(buffer buffers.NetworkBuffer) {
+	buffer.WriteString(p.Reason)
 }
--- network/packets/server/login/set_compression.go
+++ network/packets/server/login/set_compression.go
@@ -1,1 +1,15 @@
 package login
+
+import "ethene/network/buffers"
+
+type SetCompression struct {
+	Threshold int32
+}
+
+func (p *SetCompression) Id() int32 {
+	return 3
+}
+
+func (p *SetCompression) Marshal(buffer buffers.NetworkBuffer) {
+	buffer.WriteVarInt(p.Threshold)
+}
